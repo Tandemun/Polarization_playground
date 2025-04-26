33bb39e3ea2e415832e217cc0a6fdb2104b692e7
Tandemun <68216896+Tandemun@users.noreply.github.com>
2025-04-26 15:24:52 +0300

    Update geogebra-common.js

--- assets/js/geogebra-common.js
+++ assets/js/geogebra-common.js
@@ -32,3 +32,46 @@ function createGGBParams(id, material_id, overrides = {}) {
     ...overrides
   };
 }
+
+function hexToRgb(hex) {
+    hex = hex.replace('#', '');
+    let r = parseInt(hex.substring(0, 2), 16);
+    let g = parseInt(hex.substring(2, 4), 16);
+    let b = parseInt(hex.substring(4, 6), 16);
+    return [r, g, b];
+  }
+function setColors(mapping, r, g, b) {
+  mapping.forEach(function(item) {
+    item.applet.setColor(item.name, r, g, b);
+  });
+}
+  
+function syncColor(sourceApplet, sourceObjectName, targetApplet, targetObjectName) {
+    try {
+      const hexColor = sourceApplet.getColor(sourceObjectName);
+      const [r, g, b] = hexToRgb(hexColor); 
+      targetApplet.setColor(targetObjectName, r, g, b);
+      //console.log(`Synced color from ${sourceObjectName} to ${targetObjectName}: RGB(${r}, ${g}, ${b})`);
+    } catch (e) {
+      console.error(`Error syncing color:`, e);
+    }
+  }
+
+  function syncCoords(sourceApplet, sourcePointName, targetApplet, targetObjectName) {
+    try {
+      const x = sourceApplet.getXcoord(sourcePointName);
+      const y = sourceApplet.getYcoord(sourcePointName);
+      const z = sourceApplet.getZcoord(sourcePointName);
+      targetApplet.setCoords(targetObjectName, x, y, z);
+      //console.log(`Synced coords from ${sourcePointName} to ${targetObjectName}: [${x}, ${y}, ${z}]`);
+    } catch (e) {
+      console.error(`Error syncing coords:`, e);
+    }
+  }
+
+  function syncValue(sourceApplet, sourceObjectName, targetApplet,targetObjectName) {
+    // get value from controller and set value in poincare
+	const value = sourceApplet.getValue(sourceObjectName);
+	targetApplet.setValue(targetObjectName, value);
+	//console.log(`Updated ${targetObjectName} in Poincare: ${value}`);
+  } 
