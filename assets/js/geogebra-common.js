29ce0066e0bcda2dc5cd80a51cc50f01e9bb8341
Tandemun <68216896+Tandemun@users.noreply.github.com>
2025-04-28 15:02:44 +0300

    Update geogebra-common.js

--- assets/js/geogebra-common.js
+++ assets/js/geogebra-common.js
@@ -291,9 +291,11 @@ function createPoincareControl(applet, variableNames, containerId) {
   });
 
   function handleCheckboxInput(index) {
-    return function(event) {
-      const value = event.target.checked;  // Use checked property
-      const varName = variableNames[index];
+      console.log("Chekbox ${index} changed");
+      return function(event) {
+          const value = event.target.checked;  // Use checked property
+          const varName = variableNames[index];
+	  console.log("Chekbox ${index} for ${varName} changed to ${value}");
       applet.setTrace(varName, value);
     };
   }
